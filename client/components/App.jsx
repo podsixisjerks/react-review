660af740652001be28f5dc371d30868d8c7805c1
Adam Gienapp <gienappa@gmail.com>
2020-01-02 21:00:05 -0800

    "exercise 1-8 complete"

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -22,17 +22,20 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <form>
-          <label>
-            First Name:
-            <input name="firstName" onChange={this.onChangeHandler}/>
-          </label>
-          <label>
-            Last Name:
-            <input name="lastName" onChange={this.onChangeHandler}/>
-          </label>
-        </form>
-        <List changeHandler={this.onChangeHandler}/>
+        {this.state.firstName === this.props.firstName && this.state.lastName === this.props.lastName ? (
+          <List />
+        ) : (
+          <form>
+            <label>
+              First Name:
+              <input name="firstName" onChange={this.onChangeHandler}/>
+            </label>
+            <label>
+              Last Name:
+              <input name="lastName" onChange={this.onChangeHandler}/>
+            </label>
+          </form>
+        )}
       </div>
     );
   }
